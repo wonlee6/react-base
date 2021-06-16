0c02f418ca6e13724a63efc5b3b758106702925c
wonlee6 <wonlee6@gmail.com>
2021-06-16 21:29:16 +0900

    4-9 까지

--- src/App.js
+++ src/App.js
@@ -1,26 +1,10 @@
-import logo from "./logo.svg";
-import "./App.css";
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to dreload.
-        </p>
-        d
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import "./App.css";
+import Habits from "./components/habits";
+
+const App = () => {
+  return <Habits />;
+};
 
 export default App;
---
+++ src/components/habit.jsx
@@ -0,0 +1,53 @@
+import React, { Component } from "react";
+
+class Habit extends Component {
+  // state 초기값
+  // state = {
+  //   count: 0,
+  // };
+  handleIncrement = () => {
+    // state Obejct 안에 있는 count를 증가 한뒤 state를 업데이트해야됨
+    // setState 사용해야지 렌더링 됨
+    // this.setState({ count: this.state.count + 1 });
+    this.props.onIncrement(this.props.habit);
+  };
+  handleDecrement = () => {
+    // const count = this.state.count - 1;
+    // this.setState({ count: count < 0 ? 0 : count });
+    this.props.onDecrement(this.props.habit);
+  };
+  handleDelete = () => {
+    this.props.onDelete(this.props.habit);
+  };
+  render() {
+    // console.log(this.props.habit);
+    // const habitName = this.props.habit.name;
+    const { name, count } = this.props.habit;
+    return (
+      <li className="habit">
+        <span className="habit-name">{name}</span>
+        <span className="habit-count">{count}</span>
+        <button
+          className="habit-button habit-increase"
+          onClick={this.handleIncrement}
+        >
+          <i className="fas fa-plus-square"></i>
+        </button>
+        <button
+          className="habit-button habit-decrease"
+          onClick={this.handleDecrement}
+        >
+          <i className="fas fa-minus-square"></i>
+        </button>
+        <button
+          className="habit-button habit-delete"
+          onClick={this.handleDelete}
+        >
+          <i className="fas fa-trash"></i>
+        </button>
+      </li>
+    );
+  }
+}
+
+export default Habit;
