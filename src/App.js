3f7445567b7d055a7dbc8abf7f9fecd074d5ffac
wonlee6 <wonlee6@gmail.com>
2021-06-30 10:49:49 +0900

    create navbar component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,51 @@
-import React from "react";
-
+import React, { Component } from "react";
 import "./App.css";
 import Habits from "./components/habits";
+import Navbar from "./components/navbar";
 
-const App = () => {
-  return <Habits />;
-};
+class App extends Component {
+  state = {
+    habits: [
+      { id: 1, name: "Reading", count: 0 },
+      { id: 2, name: "Running", count: 0 },
+      { id: 3, name: "Coding", count: 0 },
+    ],
+  };
+  handleIncrement = (habit) => {
+    // console.log(`${habit.count}`);
+    // state를 직접적으로 사용하는 것 보단 스프레드 연산자 사용해서 하자
+    const habits = [...this.state.habits];
+    const index = habits.indexOf(habit);
+    habits[index].count++;
+    this.setState({ habits });
+  };
+  handleDecrement = (habit) => {
+    const habits = [...this.state.habits];
+    const index = habits.indexOf(habit);
+    const count = habits[index].count - 1;
+    habits[index].count = count < 0 ? 0 : count;
+    this.setState({ habits });
+  };
+  handleDelete = (habit) => {
+    // item id와 habit의 아이디르 비교하여 새로운 배열로 반환하기에 내가 선택한게 삭제한거처럼됨
+    const habits = this.state.habits.filter((item) => item.id !== habit.id);
+    this.setState({ habits });
+  };
+  render() {
+    return (
+      <>
+        <Navbar
+          totalCount={this.state.habits.filter((item) => item.count > 0).length}
+        />
+        <Habits
+          habits={this.state.habits}
+          onIncrement={this.handleIncrement}
+          onDecrement={this.handleDecrement}
+          onDelete={this.handleDelete}
+        />
+      </>
+    );
+  }
+}
 
 export default App;
--- src/components/habits.jsx
+++ src/components/habits.jsx
@@ -2,39 +2,21 @@ import React, { Component } from "react";
 import Habit from "./habit";
 
 class habits extends Component {
-  state = {
-    habits: [
-      { id: 1, name: "Reading", count: 0 },
-      { id: 2, name: "Running", count: 0 },
-      { id: 3, name: "Coding", count: 0 },
-    ],
-  };
-
   handleIncrement = (habit) => {
-    // console.log(`${habit.count}`);
-    // state를 직접적으로 사용하는 것 보단 스프레드 연산자 사용해서 하자
-    const habits = [...this.state.habits];
-    const index = habits.indexOf(habit);
-    habits[index].count++;
-    this.setState({ habits });
+    this.props.onIncrement(habit);
   };
   handleDecrement = (habit) => {
-    const habits = [...this.state.habits];
-    const index = habits.indexOf(habit);
-    const count = habits[index].count - 1;
-    habits[index].count = count < 0 ? 0 : count;
-    this.setState({ habits });
+    this.props.onDecrement(habit);
   };
   handleDelete = (habit) => {
-    // item id와 habit의 아이디르 비교하여 새로운 배열로 반환하기에 내가 선택한게 삭제한거처럼됨
-    const habits = this.state.habits.filter((item) => item.id !== habit.id);
-    this.setState({ habits });
+    this.props.onDelete(habit);
   };
 
   render() {
+    console.log(habits);
     return (
       <ul>
-        {this.state.habits.map((habit) => (
+        {this.props.habits.map((habit) => (
           <Habit
             key={habit.id}
             habit={habit}
